879bf31e850772b44f1933360124cd86c07d1728
atta-rahi <rahimoon2882@gmail.com>
2025-09-01 23:26:32 +0500

    [FAANG-0109] : Directory Setup Structure

---
+++ src/p1/java/topics/basic/HelloWorld.java
@@ -0,0 +1,9 @@
+package p1.java.topics.basic;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println(
+                "Hello World"
+        );
+    }
+}
